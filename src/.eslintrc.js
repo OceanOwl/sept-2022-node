fa90f29a81af98f6854ad8991e6c43a592eb09b9
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-03-07 22:22:57 +0100

    hw_3

---
+++ src/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
